dea24dd30cc999859627660424f479f1cdfc43e0
menoo-o <menosuper6@gmail.com>
2024-11-01 11:13:21 +0500

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Image from "next/image";
+import Posts from "./components/Posts"
 
 export default function Home() {
   return (
-    <>
-      <main className="px-6 mx-auto">
+    <main className="px-6 mx-auto">
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         Hello and Welcome 👋&nbsp;
         <span className="whitespace-nowrap">
-          I&apos;m <span className="font-bold">Meeno</span>.
+          I&apos;m <span className="font-bold">Menoo</span>.
         </span>
       </p>
-
-
+      <Posts />
     </main>
-    </>
-  );
+  )
 }
